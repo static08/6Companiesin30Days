761bf0a4f0b584e464c42083991f669c9506d79d
Priyanka <74912169+static08@users.noreply.github.com>
2022-01-03 17:53:58 +0530

    Create GS_02.cpp

---
+++ GoldmanSachs/GS_02.cpp
@@ -0,0 +1,12 @@
+#Overlapping rectangles
+
+class Solution {
+  public:
+    int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
+        if(R1[1]>L2[1] || R2[1]>L1[1])
+            return 0;
+        if(L1[0] > R2[0] || L2[0]>R1[0])
+            return 0;
+        return 1;
+    }
+};
