5e07407357c928077785db600178b229b45ce102
Priyanka <74912169+static08@users.noreply.github.com>
2022-01-04 16:24:20 +0530

    7.Find the position of M-th item

---
+++ GoldmanSachs/GS_07.cpp
@@ -0,0 +1,11 @@
+#Find the position of M-th item 
+
+class Solution {
+  public:
+    int findPosition(int N , int M , int K) {
+        if(N==1)
+            return 1;
+        int ans = (M%N)+ K-1;
+        return (ans == N)? ans: ans%N;
+    }
+};
