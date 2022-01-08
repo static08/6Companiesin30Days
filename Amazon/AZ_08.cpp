725eafa3eca03959bb97cb15be7a04271e6e6732
Priyanka <74912169+static08@users.noreply.github.com>
2022-01-08 17:42:25 +0530

    8. Count ways to N'th Stair(Order does not matter)

---
+++ Amazon/AZ_08.cpp
@@ -0,0 +1,44 @@
+#Count ways to N'th Stair(Order does not matter)
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    long long countWays(int m)
+    {
+        // your code here
+        int c = 0;
+        for(int i=0; i<=m; i++)
+        {
+            c++;
+            m--;
+        }
+        
+        return c;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking count of testcases
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        //taking stair count
+        int m;
+        cin>>m;
+        Solution ob;
+        cout<<ob.countWays(m)<<endl; // Print the output from our pre computed array
+    }
+    return 0;
+}  // } Driver Code Ends
